86757f9c863d8fb55c52c91354f88588584c5694
dunghmm <142342408+dunghmm@users.noreply.github.com>
2025-04-15 11:59:43 +0700

    Vanilla ruleset should ban j_mp_hanging_chad
    Quick fix because multiplayer_content doesn't seem to do anything for now

--- rulesets/vanilla.lua
+++ rulesets/vanilla.lua
@@ -1,7 +1,9 @@
 MP.Ruleset({
 	key = "vanilla",
 	multiplayer_content = false,
-	banned_jokers = {},
+	banned_jokers = {
+		"j_mp_hanging_chad",
+	},
 	banned_consumables = {},
 	banned_vouchers = {},
 	banned_enhancements = {},
