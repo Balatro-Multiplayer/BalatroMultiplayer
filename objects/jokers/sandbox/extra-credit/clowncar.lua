185de3eb6ee37682feb9f092d265b4ccad634ad3
Stephen Kirk <me@thestephen.net>
2025-12-18 22:46:02 +0100

    fix clown car

--- objects/jokers/sandbox/extra-credit/clowncar.lua
+++ objects/jokers/sandbox/extra-credit/clowncar.lua
@@ -11,21 +11,14 @@ SMODS.Joker({
 	atlas = "ec_jokers_sandbox",
 	pos = { x = 6, y = 2 },
 
-	config = {
-		extra = {
-			mult = 44,
-			money = 3,
-		},
-
-		mp_sticker_extra_credit = true,
-	},
+	config = { extra = { mult = 44, money = 3 }, mp_sticker_extra_credit = true },
 
 	loc_vars = function(self, info_queue, card)
 		return { vars = { card.ability.extra.mult, card.ability.extra.money } }
 	end,
 
 	calculate = function(self, card, context)
-		if context.before_but_not_as_much then
+		if context.initial_scoring_step then
 			ease_dollars(-card.ability.extra.money)
 			card_eval_status_text(
 				card,
@@ -35,14 +28,9 @@ SMODS.Joker({
 				nil,
 				{ message = "-$" .. tostring(card.ability.extra.money), colour = G.C.MONEY }
 			)
-			--Manually give +44 Mult
-			-- todo port to proper smods style
-			mult = mod_mult(mult + card.ability.extra.mult)
-			update_hand_text({ delay = 0 }, { mult = mult })
-			card_eval_status_text(card, "jokers", nil, percent, nil, {
-				message = localize({ type = "variable", key = "a_mult", vars = { card.ability.extra.mult } }),
-				mult_mod = card.ability.extra.mult,
-			})
+			return {
+				mult = card.ability.extra.mult,
+			}
 		end
 	end,
 
