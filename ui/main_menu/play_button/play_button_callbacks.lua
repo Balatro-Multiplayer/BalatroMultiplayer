d3b7a06c129278cb1e96259b53c4985ebd488df8
Stephen Kirk <me@thestephen.net>
2026-03-18 19:40:37 +0100

    prevent leak into main game

--- ui/main_menu/play_button/play_button_callbacks.lua
+++ ui/main_menu/play_button/play_button_callbacks.lua
@@ -56,22 +56,23 @@ function G.FUNCS.start_practice_run(e)
 		MP.GAME.lives = starting_lives
 		MP.GAME.enemy.lives = starting_lives
 		local deck_key = MP.UTILS.get_deck_key_from_name(r.deck)
-		if deck_key then
-			G.GAME.viewed_back = G.P_CENTERS[deck_key]
-		end
+		if deck_key then G.GAME.viewed_back = G.P_CENTERS[deck_key] end
 		G.FUNCS.start_run(e, { seed = r.seed, stake = r.stake or 1 })
-		sendDebugMessage(string.format(
-			"Practice run state: practice=%s, ghost=%s, ruleset=%s, gamemode=%s, deck_key=%s, lives=%s, enemy_lives=%s, seed=%s, stake=%s",
-			tostring(MP.is_practice_mode()),
-			tostring(MP.GHOST.is_active()),
-			tostring(MP.get_active_ruleset()),
-			tostring(MP.get_active_gamemode()),
-			tostring(deck_key),
-			tostring(MP.GAME.lives),
-			tostring(MP.GAME.enemy.lives),
-			tostring(G.GAME.pseudorandom and G.GAME.pseudorandom.seed or "?"),
-			tostring(G.GAME.stake or "?")
-		), "MULTIPLAYER")
+		sendDebugMessage(
+			string.format(
+				"Practice run state: practice=%s, ghost=%s, ruleset=%s, gamemode=%s, deck_key=%s, lives=%s, enemy_lives=%s, seed=%s, stake=%s",
+				tostring(MP.is_practice_mode()),
+				tostring(MP.GHOST.is_active()),
+				tostring(MP.get_active_ruleset()),
+				tostring(MP.get_active_gamemode()),
+				tostring(deck_key),
+				tostring(MP.GAME.lives),
+				tostring(MP.GAME.enemy.lives),
+				tostring(G.GAME.pseudorandom and G.GAME.pseudorandom.seed or "?"),
+				tostring(G.GAME.stake or "?")
+			),
+			"MULTIPLAYER"
+		)
 	else
 		G.FUNCS.setup_run(e)
 	end
@@ -148,6 +149,9 @@ end
 
 function G.FUNCS.start_lobby(e)
 	G.SETTINGS.paused = false
+
+	MP.SP.practice = false
+	MP.GHOST.clear()
 
 	MP.reset_lobby_config(true)
 
