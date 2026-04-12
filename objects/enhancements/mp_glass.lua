ddf9b7606d707063a82d3c1b6cc8cea2788c0ff6
Stephen Kirk <me@thestephen.net>
2026-04-12 20:17:16 +0200

    make display glass 1.5 (for legacy ranked etc)

--- objects/enhancements/mp_glass.lua
+++ objects/enhancements/mp_glass.lua
@@ -1,9 +1,6 @@
 MP.ReworkCenter("m_glass", {
 	rulesets = MP.UTILS.get_standard_rulesets(),
 	config = { Xmult = 2, extra = 4 },
-	in_pool = function(self, args)
-		return false
-	end,
 })
 
 MP.ReworkCenter("m_glass", {
@@ -19,7 +16,7 @@ MP.ReworkCenter("m_glass", {
 -- Display glass for ruleset descriptions — shows the reworked x2 value
 SMODS.Enhancement({
 	key = "display_glass",
-	config = { extra = { Xmult = 2, extra = 4 }, mp_sticker_balanced = true },
+	config = { extra = { Xmult = 1.5, extra = 4 }, mp_sticker_balanced = true },
 	pos = { x = 5, y = 1 },
 	no_collection = true,
 	shatters = true,
