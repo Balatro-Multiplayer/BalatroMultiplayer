286e180eec5f9b35d9fcf1712c3e424e9446e040
Stephen Kirk <me@thestephen.net>
2026-04-20 20:28:47 +0200

    Move lobby policy off classic layer onto legacy_ranked ruleset

--- layers/classic.lua
+++ layers/classic.lua
@@ -1,7 +1,5 @@
 MP.Layer("classic", {
 	multiplayer_content = false,
-	forced_gamemode = "gamemode_mp_attrition",
-	forced_lobby_options = true,
 	reworked_enhancements = {
 		"m_mp_display_glass",
 	},
